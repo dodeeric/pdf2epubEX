49c7ada3d28410e99227980156dd431220f017c1
dodeeric <eric.dodemont@skynet.be>
2020-07-08 19:07:52 +0200

    manifest for images and fonts corrected + others things

--- pdf2epubEX.sh
+++ pdf2epubEX.sh
@@ -13,8 +13,8 @@ heightincm=$(bc <<< "$heightinpts*0.0352778") # From points to cm
 
 factorratio=$(bc <<< "scale=7; ($heightinpts*1.0)/($widthinpts*1.0)")
 
-hdpi=900 # Should be named "hres"; in pixels)
-vdpi=$(bc <<< "scale=0; ($hdpi*$factorratio)/1.0") # Should be named "vres"; in pixels)
+hdpi=900 # Should be named "hres"; in pixels
+vdpi=$(bc <<< "scale=0; ($hdpi*$factorratio)/1.0") # Should be named "vres"; in pixels
 
 echo "Book/PDF Width: $width inches / $widthincm cm"
 echo "Book/PDF Height: $height inches / $heightincm cm"
@@ -41,19 +41,19 @@ read isbn
 echo -n "dc:subject (e.g.: history): "
 read tags
 
-imgformat="png"
+title="About sciences and more!"
+author="John Doe"
+publisher="O'Reilly"
+year="2020"
+lang="en"
+isbn="1234567890"
+tags="sciences"
 
-#title="About sciences and more!"
-#author="John Doe"
-#publisher="O'Reilly"
-#year="2020"
-#lang="en"
-#isbn="1234567890"
-#tags="sciences"
+imgformat="png"
 
 echo "Wait..."
 
-pdf2htmlEX --embed-css 0 --embed-font 0 --embed-image 0 --embed-javascript 0 --embed-outline 0 --split-pages 1 --bg-format $imgformat --dpi $dpi --fit-width $hdpi --fit-height $vdpi --page-filename mybook%04d.page --css-filename mybook.css mybook.pdf
+pdf2htmlEX -f 1 -l 30 --embed-css 0 --embed-font 0 --embed-image 0 --embed-javascript 0 --embed-outline 0 --split-pages 1 --bg-format $imgformat --dpi $dpi --fit-width $hdpi --fit-height $vdpi --page-filename mybook%04d.page --css-filename mybook.css mybook.pdf
 
 # Update the top and bottom of each page file
 
@@ -66,15 +66,9 @@ done
 
 # Rename each page file
 
-for g in *.page; do
-mv "$g" "`basename "$g" .page`.xhtml"
+for f in *.page; do
+mv "$f" "`basename "$f" .page`.xhtml"
 done
-
-# Update the SVG version of each background file (if SVG has been chosen)
-
-#for h in *.svg; do
-#sed -i 's/version="1.2"/version="1.1"/g' "$h"
-#done
 
 # Remove files which are not needed in the epub
 
@@ -93,15 +87,15 @@ mv *.css ./bookroot/OEBPS/
 mv *.woff ./bookroot/OEBPS/
 mv *.xhtml ./bookroot/OEBPS/
 mv *.png ./bookroot/OEBPS/
-#mv *.jpg ./bookroot/OEBPS/
-#mv *.svg ./bookroot/OEBPS/
 
 echo -n "application/epub+zip" > ./bookroot/mimetype
 
 echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<container version=\"1.0\" xmlns=\"urn:oasis:names:tc:opendocument:xmlns:container\">\n  <rootfiles>\n    <rootfile full-path=\"OEBPS/content.opf\" media-type=\"application/oebps-package+xml\"/>\n  </rootfiles>\n</container>" > bookroot/META-INF/container.xml
 
-pdfimages -png -f 1 -l 1 mybook.pdf mybook 
-mv mybook-000.png ./bookroot/OEBPS/cover.png
+# Create the cover: PDF first page --> cover.png
+
+pdfimages -png -f 3 -l 3 mybook.pdf cover
+mv cover-000.png ./bookroot/OEBPS/cover.png
 rm -f *.png
 
 # nav file
@@ -121,15 +115,15 @@ cd ../../
 
 date=$(date +%Y-%m-%dT%H:%M:%SZ)
 
-echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" prefix=\"rendition: http://www.idpf.org/vocab/rendition/#\" unique-identifier=\"pub-id\" version=\"3.0\">\n  <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n    <dc:identifier id=\"pub-id\">$isbn</dc:identifier>\n    <dc:title>$title</dc:title>\n    <dc:creator>$author</dc:creator>\n    <dc:publisher>$publisher</dc:publisher>\n    <dc:language>$lang</dc:language>\n    <dc:subject>$tags</dc:subject>\n    <dc:description>$tags</dc:description>\n    <meta content=\"cover_image\" name=\"cover\"/>\n    <meta property=\"dcterms:modified\">$date</meta>\n    <meta property=\"rendition:layout\">pre-paginated</meta>\n    <meta property=\"rendition:spread\">auto</meta>\n  </metadata>\n  <manifest>" > ./bookroot/OEBPS/content.opf
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<package xmlns=\"http://www.idpf.org/2007/opf\" prefix=\"rendition: http://www.idpf.org/vocab/rendition/#\" unique-identifier=\"pub-id\" version=\"3.0\">\n  <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n    <dc:identifier id=\"pub-id\">$isbn</dc:identifier>\n    <dc:title>$title</dc:title>\n    <dc:creator>$author</dc:creator>\n    <dc:publisher>$publisher</dc:publisher>\n    <dc:language>$lang</dc:language>\n    <dc:subject>$tags</dc:subject>\n    <dc:description>$tags</dc:description>\n    <meta content=\"cover-image\" name=\"cover\"/>\n    <meta property=\"dcterms:modified\">$date</meta>\n    <meta property=\"rendition:layout\">pre-paginated</meta>\n    <meta property=\"rendition:spread\">auto</meta>\n  </metadata>\n  <manifest>" > ./bookroot/OEBPS/content.opf
 
 cd ./bookroot/OEBPS/
 
 # 1) files
 
 for f in *.xhtml; do
-filenum=$(echo "$f" | sed 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g')
-echo -e "    <item id=\"page$filenum\" href=\"$f\" media-type=\"application/xhtml+xml\"/>" >> ./content.opf
+ff=`basename "$f" .xhtml`
+echo -e "    <item id=\"$ff\" href=\"$f\" media-type=\"application/xhtml+xml\"/>" >> ./content.opf
 done
 
 # 2) images (PNG)
@@ -137,8 +131,8 @@ done
 mv cover.png cover.xxx
 
 for f in *.png; do
-filenum=$(echo "$f" | sed 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g')
-echo -e "    <item id=\"image-page$filenum\" href=\"$f\" media-type=\"image/png\"/>" >> ./content.opf
+ff=`basename "$f" .png`
+echo -e "    <item id=\"$ff\" href=\"$f\" media-type=\"image/png\"/>" >> ./content.opf
 done
 
 mv cover.xxx cover.png
@@ -146,8 +140,8 @@ mv cover.xxx cover.png
 # 3) fonts
 
 for f in *.woff; do
-fontnum=$(echo "$f" | sed 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g')
-echo -e "    <item id=\"font$fontnum\" href=\"$f\" media-type=\"application/font-woff\"/>" >> ./content.opf
+ff=`basename "$f" .woff`
+echo -e "    <item id=\"$ff\" href=\"$f\" media-type=\"application/font-woff\"/>" >> ./content.opf
 done
 
 echo -e "    <item id=\"base-min-css\" href=\"base.min.css\" media-type=\"text/css\"/>" >> ./content.opf
@@ -155,9 +149,9 @@ echo -e "    <item id=\"mybook-css\" href=\"mybook.css\" media-type=\"text/css\"
 echo -e "    <item id=\"cover-image\" href=\"cover.png\" media-type=\"image/png\" properties=\"cover-image\"/>" >> ./content.opf
 echo -e "    <item id=\"nav\" href=\"nav.xhtml\" media-type=\"application/xhtml+xml\" properties=\"nav\"/>\n  </manifest>\n  <spine>" >> ./content.opf
 
-for g in *.xhtml; do
-pagenum=$(echo "$g" | sed 's/[^0-9][^0-9]*\([0-9][0-9]*\).*/\1/g')
-echo -e "    <itemref idref=\"page$pagenum\" properties=\"rendition:layout-pre-paginated\"/>" >> ./content.opf
+for f in *.xhtml; do
+ff=`basename "$f" .xhtml`
+echo -e "    <itemref idref=\"$ff\" properties=\"rendition:layout-pre-paginated\"/>" >> ./content.opf
 done
 
 echo -e "  </spine>\n  <guide>\n    <reference type=\"cover\" title=\"Cover\" href=\"mybook0001.xhtml\"/>\n    <reference type=\"text\" title=\"Text\" href=\"mybook0002.xhtml\"/>\n  </guide>\n</package>" >> ./content.opf
